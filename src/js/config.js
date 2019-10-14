5836cf51946db76cc92e0c1f17822a815718f70a
DeveloperAdam-github <56017774+DeveloperAdam-github@users.noreply.github.com>
2019-10-14 19:00:14 +0100

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '27c61891f2d5fda00e7fb4e57bd9ebb0';
+
+//27c61891f2d5fda00e7fb4e57bd9ebb0
+//439d028911c868329b827a70c01309d9
